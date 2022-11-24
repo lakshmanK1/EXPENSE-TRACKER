9643be80c854a6193485b66cac3f7348b7507b40
lakshmanK1 <laxmankamisetty1@gmail.com>
2022-11-24 10:02:25 +0530

    update profile details and get back details after refresh

--- src/Components/Pages/Updatadetails.js
+++ src/Components/Pages/Updatadetails.js
@@ -1,76 +1,137 @@
-import React,{useContext, useRef} from 'react'
-import {BsPersonCircle, BsGlobe2} from 'react-icons/bs'
-import { Link } from 'react-router-dom'
-import { AuthenticationContext } from '../Store/AuthContext';
+import React, { useContext, useRef, useState, useEffect } from "react";
+import { BsPersonCircle, BsGlobe2 } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { AuthenticationContext } from "../Store/AuthContext";
 
 // styled cmps
-import { DetailsForm, DetailsLabel, DetailsInput, DetailsButton, DetailsContainer, Heading, CloseButton } from './PageStyledComponents'
+import {
+  DetailsForm,
+  DetailsLabel,
+  DetailsInput,
+  DetailsButton,
+  DetailsContainer,
+  Heading,
+  CloseButton,
+} from "./PageStyledComponents";
 
 function Updatadetails() {
+  const UserInputName = useRef();
+  const UserInputUrl = useRef();
 
-    const UserInputName = useRef();
-    const UserInputUrl = useRef();
+  const [displayName, setDisplayName] = useState('');
+  const [urlLink, setUrlLink] = useState('');
 
-    const AuthCntx = useContext(AuthenticationContext);
+  const AuthCntx = useContext(AuthenticationContext);
 
-    const handleForm = (e) => {
-        e.preventDefault();
+  const handleForm = (e) => {
+    e.preventDefault();
 
-        var enteredName = UserInputName.current.value;
-        var enteredUrl = UserInputUrl.current.value;
+    var enteredName = UserInputName.current.value;
+    var enteredUrl = UserInputUrl.current.value;
 
-    let url='https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyB8PZ7dyMV1RlPAiConsdPAQszUAEsecfI';
-        fetch(url,{
-            method:"POST",
-            body:JSON.stringify({
-                idToken:AuthCntx.token,
-                displayName:enteredName,
-                photoUrl:enteredUrl,
-                returnSecureToken:true,
-              }),
-            headers:{
-                'Content-Type':'application/json'
-            }}).then((res)=>{
-                if(res.ok){
-                    return res.json();
-                }else{
-                    return res.json().then((data)=>{
-                        if(data && data.error && data.error.message){
-                            let errMessage = "Authentication Failed, " + data.error.message;
-                            throw new Error(errMessage);
-                        }
-                    })
+    let url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyB8PZ7dyMV1RlPAiConsdPAQszUAEsecfI";
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        idToken: localStorage.getItem("token"),
+        displayName: enteredName,
+        photoUrl: enteredUrl,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res)=>{
+           
+        if(res.ok){
+            return res.json();
+        }else{
+            return res.json().then((data)=>{
+                if(data && data.error && data.error.message){
+                    let errMessage = "Authentication Failed, " + data.error.message;
+                    throw new Error(errMessage);
                 }
-        }).then((data)=>{
-            console.log(data);
-        }).catch((err)=>{
-            alert(err.message);
-        })
+            })
+        }
+    }).then((data)=>{
+        console.log(data);
+    })
+      .then(() => {
+        fetch(
+          "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyB8PZ7dyMV1RlPAiConsdPAQszUAEsecfI",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              idToken: localStorage.getItem("token"),
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-        var enteredName = UserInputName.current.value='';
-        var enteredUrl = UserInputUrl.current.value='';
+    // var enteredName = UserInputName.current.value='';
+    // var enteredUrl = UserInputUrl.current.value='';
+  };
 
+  async function getBackData() {
+    console.log("Async Function");
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyB8PZ7dyMV1RlPAiConsdPAQszUAEsecfI",
+      {
+        method: "POST",
+        body: JSON.stringify({
+        idToken: localStorage.getItem("token"),
+        }),
+      }
+    );
 
-    }
+    const data = await response.json();
+    const userData = data.users;
+    console.log(userData);
+    setDisplayName(userData[0].displayName);
+    setUrlLink(userData[0].photoUrl);
+  }
+
+  useEffect(() => {
+    getBackData();
+  }, []);
 
   return (
     <DetailsContainer>
-        <Heading>Contact Details</Heading>
-        <Link to='/welcomepage'><CloseButton>cancel</CloseButton></Link>
-        <DetailsForm onSubmit={handleForm}>
-            <div style={{margin:'8px'}}>
-            <DetailsLabel><BsPersonCircle style={{width:'25px',height:'18px'}}/>Full Name: </DetailsLabel>
-            <DetailsInput type='text' ref={UserInputName} required/>
-            </div>
-            <div style={{margin:'8px'}}>
-            <DetailsLabel><BsGlobe2 style={{width:'25px',height:'18px'}}/>Profile Photo Url: </DetailsLabel>
-            <DetailsInput type='text' ref={UserInputUrl} required/>
-            </div><br/>
-        
-            <DetailsButton>update</DetailsButton>
-        </DetailsForm>
+      <Heading>Contact Details</Heading>
+      <Link to="/welcomepage">
+        <CloseButton>cancel</CloseButton>
+      </Link>
+      <DetailsForm onSubmit={handleForm}>
+        <div style={{ margin: "8px" }}>
+          <DetailsLabel>
+            <BsPersonCircle style={{ width: "25px", height: "18px" }} />
+            Full Name:{" "}
+          </DetailsLabel>
+          <DetailsInput type="text" ref={UserInputName} defaultValue={displayName} required />
+        </div>
+        <div style={{ margin: "8px" }}>
+          <DetailsLabel>
+            <BsGlobe2 style={{ width: "25px", height: "18px" }} />
+            Profile Photo Url:{" "}
+          </DetailsLabel>
+          <DetailsInput type="text" ref={UserInputUrl} defaultValue={urlLink} required />
+        </div>
+        <br />
+
+        <DetailsButton>update</DetailsButton>
+      </DetailsForm>
     </DetailsContainer>
-  )
+  );
 }
 
-export default Updatadetails
+export default Updatadetails;
