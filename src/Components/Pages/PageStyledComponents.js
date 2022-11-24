543c66e4f2e36bb67707f3fc4fa54e62c0a78d1a
lakshmanK1 <laxmankamisetty1@gmail.com>
2022-11-24 11:04:06 +0530

    added verify email Functionality

--- src/Components/Pages/PageStyledComponents.js
+++ src/Components/Pages/PageStyledComponents.js
@@ -9,6 +9,8 @@ export const MessageDiv = styled.div`background-color:#D6E4E5; width:350px;
 border-radius:5px; padding:5px; float:right; text-align:center; margin-right:15px;`;
 export const Span = styled.span`color:blue; font-style:italic; cursor:pointer;`;
 export const Text = styled.p``;
+export const EmailVerifyBTN = styled.button`float:right; font-size:22px;color:#2192ff; border:2px solid #2192ff; 
+background-color:white; padding:10px; font-weight:600; margin-right:15px; width:180px; `;
 
 
 
--- src/Components/Pages/WelcomePage.js
+++ src/Components/Pages/WelcomePage.js
@@ -1,14 +1,51 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { Container, HeaderText, MessageDiv, Span, Text } from './PageStyledComponents'
+import { AuthenticationContext } from '../Store/AuthContext'
+import { Container, HeaderText, MessageDiv, Span, Text, EmailVerifyBTN } from './PageStyledComponents'
 
 function WelcomePage() {
+
+  const AuthCntx = useContext(AuthenticationContext);
+
+  const VerifyEmailId = () => {
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyB8PZ7dyMV1RlPAiConsdPAQszUAEsecfI',{
+      method:"POST",
+      body:JSON.stringify({
+        requestType:"VERIFY_EMAIL",
+        idToken:AuthCntx.token
+      }),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    }).then((res)=>{
+           
+      if(res.ok){
+          return res.json();
+      }else{
+          return res.json().then((data)=>{
+              if(data && data.error && data.error.message){
+                  let errMessage = "Authentication Failed, " + data.error.message;
+                  throw new Error(errMessage);
+              }
+          })
+      }
+  }).then((data)=>{
+    console.log(data);
+  }).catch((err)=>{
+    alert(err.message);
+  })
+  }
+
   return (
     <Container>
         <MessageDiv>
             <Text>your profile is Incomplete,<Link to='/welcomepage/updateUserdetails'><Span>Complete now</Span></Link></Text>
         </MessageDiv>
-        <HeaderText>Welcome to Expense Tracker Application</HeaderText>
+        
+
+        <HeaderText>Welcome to Expense Tracker Application</HeaderText>         
+        <EmailVerifyBTN onClick={VerifyEmailId}>Verify Email</EmailVerifyBTN>
+
         <hr/>
     </Container>
   )
